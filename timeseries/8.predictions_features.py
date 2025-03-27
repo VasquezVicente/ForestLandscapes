e64effa5fc53e3351e17413b7f3b0128aacb6703
vicente <vicenteavv7@gmail.com>
2025-03-27 12:16:20 -0500

    quit backing up the csv, getting too heavy, phenological metrics updated to PELT

--- timeseries/8.predictions_features.py
+++ timeseries/8.predictions_features.py
@@ -5,25 +5,22 @@ import rasterio
 from shapely import box
 import matplotlib.pyplot as plt
 import shapely
-from rasterio.mask import mask
 import numpy as np
 from skimage.filters.rank import entropy
-from skimage.morphology import disk
 import pickle
 from rasterio.mask import mask
 import shapely
-import numpy as np
-import matplotlib.pyplot as plt
-from shapely.geometry import box
 import matplotlib.patches as patches
 from shapely.affinity import affine_transform
 from matplotlib.backends.backend_pdf import PdfPages
 from skimage.feature import graycomatrix, graycoprops
 from skimage import img_as_ubyte
-import seaborn as sns
-from skimage.filters.rank import entropy
 from skimage.morphology import disk
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))  # Add script's directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # Add parent directory
 
+from timeseries.utils import calculate_glcm_features
 
 #load polygons
 data_path=r"\\stri-sm01\ForestLandscapes\UAVSHARE\BCI_50ha_timeseries"
@@ -33,7 +30,8 @@ crowns=gpd.read_file(path_crowns)
 crowns['polygon_id']= crowns['GlobalID']+"_"+crowns['date'].str.replace("_","-")
 
 
-dipteryx_subset= crowns[crowns['latin']=='Dipteryx oleifera'].reset_index()
+species_subset= crowns[crowns['latin']=='Hura crepitans'].reset_index()
+
 pixel_unmixing=gpd.read_file(os.path.join(data_path,'aux_files/pixel_unmixing.shp'))
 gv_pixels = []  # For GV (Green Vegetation)
 npv_pixels = []  # For NPV (Non-photosynthetic Vegetation)
@@ -75,37 +73,15 @@ A = np.vstack([gv_endmember, npv_endmember,shadow_endmember]).T
 A_inv = np.linalg.pinv(A)
 A_inv.shape
 
-plt.figure(figsize=(8, 6))
-bands = ['Red', 'Green', 'Blue']
-plt.plot(bands, gv_endmember, label='GV Endmember (Green Vegetation)', marker='o')
-plt.plot(bands, npv_endmember, label='NPV Endmember (Non-Photosynthetic Vegetation)', marker='o')
-plt.plot(bands, shadow_endmember, label='Shadow Endmember (Shadow)', marker='o')
-
-plt.xlabel('Spectral Bands')
-plt.ylabel('Reflectance Value')
-plt.title('Spectral Response of Endmembers')
-plt.legend()
-plt.grid(True)
-plt.show()
-
 
 #angles for covariance matrix
 window_size = 5  # 5x5 window
 angles = [0, 45, 90, 135]  # Azimuths in degrees
 
-def calculate_glcm_features(image, window_size=5, angles=[0, 45, 90, 135]):
-    glcm_features = []
-    for angle in angles:
-        # Calculate the GLCM for the specified angle
-        glcm = graycomatrix(img_as_ubyte(image), distances=[1], angles=[np.deg2rad(angle)], symmetric=True, normed=True) 
-        correlation = graycoprops(glcm, 'correlation')[0, 0]
-        glcm_features.append(correlation)
-    return glcm_features
-
 
 #extract the features, crown based 
-for i, (_, row) in enumerate(dipteryx_subset.iterrows()):
-    print(f"Processing iteration {i + 1} of {len(dipteryx_subset)}")
+for i, (_, row) in enumerate(species_subset.iterrows()):
+    print(f"Processing iteration {i + 1} of {len(species_subset)}")
     path_orthomosaic = os.path.join(data_path,'orthomosaic_aligned_local', f"BCI_50ha_{row['date']}_local.tif")
     with rasterio.open(path_orthomosaic) as src:
         out_image, out_transform = mask(src, [row.geometry], crop=True)
@@ -160,64 +136,24 @@ for i, (_, row) in enumerate(dipteryx_subset.iterrows()):
         entropyM=np.nanmean(entropy_image)
         elevSD= np.nanstd(elev)
 
-        dipteryx_subset.at[i, 'rccM'] = rccM
-        dipteryx_subset.at[i, 'gccM'] = gccM
-        dipteryx_subset.at[i, 'bccM'] = bccM
-        dipteryx_subset.at[i, 'ExGM'] = ExGM
-        dipteryx_subset.at[i, 'gvM'] = gvM
-        dipteryx_subset.at[i, 'npvM'] = npvM
-        dipteryx_subset.at[i, 'shadowM'] = shadowM
-        dipteryx_subset.at[i, 'rSD'] = rSD
-        dipteryx_subset.at[i, 'gSD'] = gSD
-        dipteryx_subset.at[i, 'bSD'] = bSD
-        dipteryx_subset.at[i, 'ExGSD'] = ExGSD
-        dipteryx_subset.at[i, 'gvSD'] = gvSD
-        dipteryx_subset.at[i, 'npvSD'] = npvSD
-        dipteryx_subset.at[i, 'gcorSD'] = gcorSD
-        dipteryx_subset.at[i, 'gcorMD'] = gcorMD
-        dipteryx_subset.at[i, 'entropy']= entropyM
-        dipteryx_subset.at[i, 'elevSD']= elevSD
+        species_subset.at[i, 'rccM'] = rccM
+        species_subset.at[i, 'gccM'] = gccM
+        species_subset.at[i, 'bccM'] = bccM
+        species_subset.at[i, 'ExGM'] = ExGM
+        species_subset.at[i, 'gvM'] = gvM
+        species_subset.at[i, 'npvM'] = npvM
+        species_subset.at[i, 'shadowM'] = shadowM
+        species_subset.at[i, 'rSD'] = rSD
+        species_subset.at[i, 'gSD'] = gSD
+        species_subset.at[i, 'bSD'] = bSD
+        species_subset.at[i, 'ExGSD'] = ExGSD
+        species_subset.at[i, 'gvSD'] = gvSD
+        species_subset.at[i, 'npvSD'] = npvSD
+        species_subset.at[i, 'gcorSD'] = gcorSD
+        species_subset.at[i, 'gcorMD'] = gcorMD
+        species_subset.at[i, 'entropy']= entropyM
+        species_subset.at[i, 'elevSD']= elevSD
 
-dipteryx_subset=dipteryx_subset.drop(columns=['geometry'])
+species_subset=species_subset.drop(columns=['geometry'])
 
-dipteryx_subset.to_csv(r"timeseries/dataset_predictions/dipteryx_sgbt.csv")
-
-dipteryx_predict= pd.read_csv(r'timeseries/dataset_predictions/dipteryx_sgbt.csv')
-dipteryx_predict.columns
-
-with open(r'timeseries/models/xgb_model.pkl', 'rb') as file:
-      model = pickle.load(file)
-
-X=dipteryx_predict[['rccM', 'gccM', 'bccM', 'ExGM', 'gvM', 'npvM', 'shadowM','rSD', 'gSD', 'bSD',
-       'ExGSD', 'gvSD', 'npvSD', 'gcorSD', 'gcorMD','entropy','elevSD']]
-
-Y= dipteryx_predict[['area', 'score', 'tag', 'GlobalID', 'iou',
-       'date', 'latin', 'polygon_id']]
-X_predicted=model.predict(X)
-df_final = Y.copy()  # Copy Y to keep the same structure
-df_final['leafing_predicted'] = X_predicted
-
-df_final['date'] = pd.to_datetime(df_final['date'], format='%Y_%m_%d')
-df_final.columns
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df_final = df_final.sort_values(by=['GlobalID', 'date'])
-
-# Set plot size
-plt.figure(figsize=(12, 6))
-
-# Plot one line per GlobalID
-sns.lineplot(data=df_final, x='date', y='leafing_predicted', hue='GlobalID', estimator=None, lw=2)
-
-# Formatting
-plt.xlabel('Date')
-plt.ylabel('Leafing Predicted')
-plt.title('Leafing Predictions Over Time for Each GlobalID')
-plt.xticks(rotation=45)
-plt.legend(title='GlobalID', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.grid(True)
-
-# Show the plot
-plt.show()
+species_subset.to_csv(r"timeseries/dataset_predictions/hura_sgbt.csv")
--- timeseries/9.extract_phenology_metrics.py
+++ timeseries/9.extract_phenology_metrics.py
@@ -7,7 +7,7 @@ import ruptures as rpt
 import statsmodels.api as sm
 
 
-data= pd.read_csv(r"timeseries/dataset_predictions/dipteryx_sgbt.csv")
+data= pd.read_csv(r"timeseries/dataset_predictions/hura_sgbt.csv")
 
 with open(r'timeseries/models/xgb_model.pkl', 'rb') as file:
       model = pickle.load(file)
@@ -27,6 +27,31 @@ df_final['year']= df_final['date'].dt.year
 df_final['date_num']= (df_final['date'] -df_final['date'].min()).dt.days
 
 globalID= df_final['GlobalID'].unique()
+len(globalID)
+plt.figure(figsize=(20, 6))
+for tree in df_final['GlobalID'].unique():
+    indv = df_final[df_final['GlobalID'] == tree]
+    tagn= indv['tag'].unique()[0]
+    plt.plot(indv['date'], indv['leafing_predicted'], label=f"Tree {tagn}", alpha=0.7)
+plt.xlabel('Date')
+plt.ylabel('Leafing Predicted')
+plt.grid(True)
+plt.title('Hura Crepitans: Leaf coverage vs Date')
+plt.legend(title="ForestGeo Tag", bbox_to_anchor=(1, 1), loc='upper left')
+plt.show()
+
+
+df_final['tag_numeric'] = pd.Categorical(df_final['tag']).codes
+lowess = sm.nonparametric.lowess(df_final['leafing_predicted'], df_final['dayYear'], frac=0.09)  # Adjust frac for smoothing level
+plt.plot(lowess[:, 0], lowess[:, 1], color='red', linewidth=2, label="LOWESS Trend")
+plt.scatter(df_final['dayYear'], df_final['leafing_predicted'], c=df_final['tag_numeric'], cmap='viridis', alpha=0.7)
+
+plt.xlabel('Day of Year')
+plt.ylabel('Leafing Predicted')
+plt.grid(True)
+plt.title('Leafing Predicted vs Day of Year with Trend Line')
+plt.show()
+
 
 all_df = []
 for tree in globalID:
@@ -38,111 +63,87 @@ for tree in globalID:
     full_df['year'] = full_df['date'].dt.year
     indv = pd.merge(full_df, indv, on=['date', 'date_num', 'year', 'dayYear'], how='left')
     indv['leafing_predicted'] = indv['leafing_predicted'].interpolate(method='linear')
+
+    #plt.figure(figsize=(12, 6))
+    #plt.scatter(indv['dayYear'], indv['leafing_predicted'], c=indv['year'], cmap='viridis', label='Leafing Predicted')
+    #plt.xlabel('Day of year')
+    #plt.ylabel('Leafing Predicted')
+    #plt.grid(True)
+    #plt.title('Leafing Predicted vs Day of Year')
+    #plt.legend()
+    #plt.colorbar(label='Year')
+    #plt.show()
+
     signal = indv['leafing_predicted'].values
-    algo_python = rpt.Pelt(model="rbf", jump=1, min_size=5).fit(signal)
+    algo_python = rpt.Pelt(model="rbf",jump=1,min_size=15).fit(signal)
     penalty_value = 30
     bkps_python = algo_python.predict(pen=penalty_value)
+
+
+    #plt.figure(figsize=(12, 6))
+    #plt.scatter(indv['date_num'], indv['leafing_predicted'], c=indv['year'], cmap='viridis', label='Leafing Predicted')
+    #plt.xlabel('Day of year')
+    #plt.ylabel('Leafing Predicted')
+    #plt.grid(True)
+    #plt.title('Leafing Predicted vs Day of Year')
+    #plt.legend()
+    #plt.colorbar(label='Year')
+    #for bp in bkps_python:
+    #    plt.axvline(x=bp, color='red', linestyle='--', linewidth=2)
+    #plt.show()
+
     indv['breakpoint'] = False
     indv.loc[indv['date_num'].isin(bkps_python), 'breakpoint'] = True
     indv['GlobalID'] = tree
     all_df.append(indv)
+    print("one done")
 
 out_df = pd.concat(all_df, ignore_index=True)
 
 break_group= out_df[out_df['breakpoint']==True]
+break_group[break_group['dayYear'].isna()]
 
-
-plt.figure(figsize=(10, 6))
-plt.hist(break_group['dayYear'], bins=365, color='blue', edgecolor='black', alpha=0.7)
-plt.xlabel('Day of the Year')
-plt.ylabel('Breakpoint Count')
-plt.title('Concentration of Breakpoints by Day of the Year')
-plt.grid(True)
+plt.scatter(break_group['year'], break_group['dayYear'])
 plt.show()
 
 
 
-globalID= all['GlobalID'].unique()
+# I am goint to have to extract the species specific metric
+# maybe i can do that with the etire dataset, then filter the trees that do not match the pattern(noise)
+# i am going to have to aggregate the dates
 
-indv= all[all['GlobalID']=='badee35d-7b09-4d87-9b4a-b052af7c3749']
+df = df_final.groupby('date', as_index=False)['leafing_predicted'].mean()
+full_date_range = pd.date_range(start=df_final['date'].min(), end=df_final['date'].max(), freq='D')
+full_df = pd.DataFrame({'date': full_date_range})
+full_df['dayYear'] = full_df['date'].dt.dayofyear
+full_df['date_num'] = (full_df['date'] - full_df['date'].min()).dt.days
+full_df['year'] = full_df['date'].dt.year
+full = pd.merge(full_df, df, on=['date'], how='left')
+full['leafing_predicted'] = full['leafing_predicted'].interpolate(method='linear')
 
-
-
-for tree in globalID:
-     indv= all[all['GlobalID']==tree].reset_index()
-     indv['leafing_predicted'] = indv['leafing_predicted'].interpolate(method='linear')
-
-     
-
-plt.figure(figsize=(12, 6))
-plt.scatter(indv.dropna(subset=['leafing_predicted'])['date_num'],
-            indv.dropna(subset=['leafing_predicted'])['leafing_predicted'],
-            c=indv.dropna(subset=['leafing_predicted'])['year'], cmap='viridis')
-plt.xlabel('Day of year')
-plt.ylabel('Leafing Predicted')
-plt.grid(True)
-plt.title('Leafing Predicted vs Day of Year')
-plt.legend()
-plt.colorbar(label='Year')
+plt.scatter(full['date'], full['leafing_predicted'])
 plt.show()
 
-
-
-
-
-
-
-
-# Fit LOWESS smoothing
-lowess = sm.nonparametric.lowess(indv['leafing_predicted'], indv['date_num'], frac=0.1)  # frac controls smoothness
-
-# Plot scatter and LOWESS trend
-plt.figure(figsize=(12, 6))
-plt.scatter(indv['date_num'], indv['leafing_predicted'], c=indv['year'], cmap='viridis', label='Leafing Predicted', alpha=0.6)
-plt.plot(lowess[:, 0], lowess[:, 1], color='red', linewidth=2, label='LOWESS Trend')
-
-plt.xlabel('Day of year')
-plt.ylabel('Leafing Predicted')
-plt.grid(True)
-plt.title('Leafing Predicted vs Day of Year with LOWESS Trend Line')
-plt.legend()
-plt.colorbar(label='Year')
-plt.show()
-
-
-# Extract the signal
-#for year 2019
-
-signal = indv['leafing_predicted'].values  # Get the leafing predictions
-len(signal)
-# Apply rupture's dynamic programming method
-algo_python = rpt.Pelt(model="rbf", jump=1, min_size=5).fit(
-    signal
-)
-penalty_value=30
+signal = full['leafing_predicted'].values
+algo_python = rpt.Pelt(model="rbf",jump=1,min_size=15).fit(signal)
+penalty_value = 30
 bkps_python = algo_python.predict(pen=penalty_value)
 
 
-
-# Plot the leafing predictions
 plt.figure(figsize=(12, 6))
-plt.scatter(indv['date_num'], indv['leafing_predicted'], c=indv['year'], cmap='viridis', label='Leafing Predicted')
-plt.xlabel('Day of year')
-plt.ylabel('Leafing Predicted')
+plt.scatter(full['date_num'], full['leafing_predicted'], c=full['year'], cmap='viridis', label='Leafing Predicted')
+plt.xlabel('Date numeric')
+plt.ylabel('Mean leaf coverage')
 plt.grid(True)
-plt.title('Leafing Predicted vs Day of Year')
+plt.title('Hura crepitans mean leaf coverage vs date')
 plt.legend()
 plt.colorbar(label='Year')
 
-# Add the detected breakpoints as dashed vertical lines
 for bp in bkps_python:
     plt.axvline(x=bp, color='red', linestyle='--', linewidth=2)
-
-# Show the plot
+    date_label = full.loc[full['date_num'] == bp, 'date'].dt.date.values  # Extract only date part
+    if len(date_label) > 0:
+        plt.text(bp, full['leafing_predicted'].mean(), str(date_label[0]), 
+                 rotation=45, color='black', fontsize=10, verticalalignment='center_baseline')
 plt.show()
-
-
-indv['breakpoint'] = False  
-indv.loc[indv['date_num'].isin(bkps_python), 'breakpoint'] = True
-
-indv[indv['breakpoint']==True]
+bkps_python
