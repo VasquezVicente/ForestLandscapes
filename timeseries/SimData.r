403ecc922640d6e96ce691c0265607b3281129af
Vicente <vicenteavv7@gmail.com>
2026-01-27 10:49:06 -0500

    Backing up to new processing unit

--- timeseries/SimData.r
+++ timeseries/SimData.r
@@ -1,5 +1,239 @@
 library(pacman)
-p_load(dclone, MASS, ggplot2, snow)
+p_load(dclone, MASS, ggplot2, snow, tidyverse)
+
+n.years  <- 7
+one.year <- seq(from=1,to=365,by=30)
+samp.days <- rep(one.year,n.years)
+n.inds <- 10
+all.days <- rep(samp.days,n.inds)
+n <- length(all.days)
+year.id  <- rep(rep(1:n.years, each = length(one.year)), n.inds)
+indv.id  <- rep(1:n.inds, each = length(samp.days))
+
+logit.pf <- function(kd,Td,x){
+  out <- kd*(x-Td)
+  return(out)
+}
+
+sigsq <- 0.45  #noise levels 
+kd <- 0.1
+Td <- 100
+mu.true <- logit.pf(kd=kd,Td=Td,x=all.days)
+
+norm.samps <- rnorm(n=n, mean=mu.true, sd=sqrt(sigsq))
+y.sims <- 1/(1+exp(norm.samps))
+
+df<- data.frame(
+  days=all.days,
+  indv=indv.id,
+  year=year.id,
+  y=y.sims
+)
+
+windows()
+ggplot(df, aes(x=days, y=y, color=as.factor(year))) +
+  geom_point() +
+  labs(title="Simulated phenology data",
+       y="Simulated y",
+       x="Day of year") +
+  theme_minimal()
+
+
+##JAGS model for intercept
+leaves <- function(){
+  lkd ~ dnorm(0,0.4)
+  kd <- exp(lkd)
+  ltd ~ dnorm(0,4)
+  Td <- exp(ltd)
+  ls ~ dnorm(0,1)
+  sigsq <- pow(exp(ls),2)
+  for(j in 1:n){
+    muf[j] <-  kd*(days[j]-Td)
+  }
+  for(k in 1:K){
+    for(i in 1:n){
+      X[i,k] ~ dnorm(muf[i],1/sigsq)
+    } 
+  }
+}
+
+
+
+test.data <- log(1-y.sims) - log(y.sims)
+data4dclone <- list(K=1, X=dcdim(data.matrix(test.data)), n=n, days=all.days)
+
+cl.seq <- c(1,4,8,16);
+n.iter<-10000;n.adapt<-5000;n.update<-100;thin<-10;n.chains<-3;
+
+out.parms <- c("kd", "Td", "sigsq")
+leaves.dclone <- dc.fit(data4dclone, params=out.parms, model=leaves, n.clones=cl.seq,
+                        multiply="K",unchanged="n",
+                        n.chains = n.chains, 
+                        n.adapt=n.adapt, 
+                        n.update=n.update,
+                        n.iter = n.iter, 
+                        thin=thin,
+                        inits=list(lkd=log(0.2), ltd=log(40))
+                        )
+
+dcdiag(leaves.dclone)
+summary(leaves.dclone)
+
+dctable <- dctable(leaves.dclone)
+windows()
+plot(dctable)
+windows()
+plot(dctable, type="log.var")
+
+
+#lets try real data with the same model
+
+
+data<- read.csv("C:\\Users\\Vicente\\repo\\ForestLandscapes\\timeseries\\dataset_extracted\\cavallinesia.csv")
+head(data)
+
+data <- data %>%
+  mutate(
+    y_norm= pmin(pmax(leafing / 100, 1e-4), 1 - 1e-4),
+    date = as.Date(date),
+    date_num = as.numeric(difftime(date, as.Date("2018-04-04"), units = "days")),
+    DOY= yday(date),
+    year= year(date),
+    month= month(date),
+    pheno_year = if_else(month >= 9, year, year - 1),
+    day = as.numeric(difftime(date, as.Date(paste0(pheno_year, "-09-01")), units = "days")),
+    tree= as.factor(tag),
+    pheno_year= as.factor(pheno_year),
+    tree_year= as.factor(paste0(tree, "_", pheno_year))
+  )
+
+
+
+trees<- unique(data$tree)
+years<- unique(data$pheno_year)
+all_before_threshold <- data.frame()
+for (i in 1:length(trees)) {
+  for (j in 1:length(years)) {
+    subset_data <- data %>% filter(tree == trees[i], pheno_year == years[j])
+    subset_data<- subset_data %>% arrange(day)
+    windows()
+    plot(subset_data$day, subset_data$y_norm, type='l', main=paste("Tree:", trees[i], "Year:", years[j]),
+         xlab="Day", ylab="Normalized Leafing")
+    #find values below threshold
+    threshold <- 0.2
+    below_threshold <- which(subset_data$y_norm < threshold)
+    last_item<-max(below_threshold)
+    #subset all values before last_item
+    if (length(below_threshold) > 0 && last_item < nrow(subset_data)) {
+      subset_before_threshold <- subset_data[1:last_item, ]
+      points(subset_before_threshold$day, subset_before_threshold$y_norm, col='red', pch=19)
+      all_before_threshold <- bind_rows(all_before_threshold, subset_before_threshold)
+    }
+      
+  }}
+
+
+
+windows()
+ggplot(all_before_threshold, aes(x=day, y=y_norm, color=as.factor(tree_year))) +
+  geom_point() +
+  labs(title="Cavallinesia phenology data",
+       y="Predicted leafing",
+       x="Day of year") +
+  theme_minimal()
+
+#logit transform 
+test.data <- log(1-all_before_threshold$y_norm) - log(all_before_threshold$y_norm)
+data4dclone <- list(K=1, X=dcdim(data.matrix(test.data)), n=nrow(all_before_threshold), days=all_before_threshold$day)
+cl.seq <- c(1,4,8,16);
+n.iter<-10000;n.adapt<-5000;n.update<-100;thin<-10;n.chains<-3;
+out.parms <- c("kd", "Td", "sigsq")
+cava.intercept <- dc.fit(data4dclone, params=out.parms, model=leaves, n.clones=cl.seq,
+                        multiply="K",unchanged="n",
+                        n.chains = n.chains, 
+                        n.adapt=n.adapt, 
+                        n.update=n.update,
+                        n.iter = n.iter, 
+                        thin=thin,
+                        inits=list(lkd=log(0.2), ltd=log(40))
+)
+
+
+summary(cava.intercept)
+
+dcdiag(cava.intercept)
+cavatable <- dctable(cava.intercept)
+windows()
+plot(cavatable)
+windows()
+plot(cavatable, type="log.var")
+logit.pf(kd=coef(cava.intercept)["kd"],Td=coef(cava.intercept)["Td"],x=seq(1,365,1))->LC_values
+LC_values<- 1/(1+exp(LC_values))
+
+x <- seq(1, 365)
+
+calendar_doy <- ((x + 243 - 1) %% 365) + 1
+
+results_df <- data.frame(
+  x = x,
+  y = LC_values,
+  doy = calendar_doy,
+  date= as.Date(calendar_doy - 1, origin = "2018-01-01")
+)
+head(results_df)
+
+#what date is 90%
+results_df %>%
+  filter(y <= 0.9) %>%
+  slice(1)
+
+results_df %>%
+  filter(y <= 0.5) %>%
+  slice(1)
+
+results_df %>%
+  filter(y <= 0.1) %>%
+  slice(1)
+
+day_date<- seq(from=as.Date("2018-09-01"), to=as.Date("2019-08-31"), by="month")
+day_date_lookup <- data.frame(
+  day = as.numeric(difftime(day_date, as.Date("2018-09-01"), units = "days")),
+  date = day_date
+)
+
+
+windows()
+ggplot(all_before_threshold,
+       aes(x = day, y = y_norm, color = as.factor(tree_year))) +
+  geom_point() +
+  geom_line(data = results_df, aes(x = x, y = y),
+            color = "black", linewidth = 1) +
+  scale_x_continuous(
+    breaks = day_date_lookup$day,
+    labels = format(day_date_lookup$date, "%b %d")
+  ) +
+  labs(
+    title = "Cavallinesia phenology data",
+    y = "Predicted leafing",
+    x = "Date"
+  ) +theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  geom_vline(xintercept = c(87,126, 165), linetype = "dashed", color = "black")
+  
+
+#lets show it circularly
+data<- data %>%
+  mutate(
+    is_leap = if_else(year %% 4 == 0 & (year %% 100 != 0 | year %% 400 == 0), TRUE, FALSE),
+    rad = if_else(is_leap, 2 * pi * DOY / 366, 2 * pi * DOY / 365)
+  )
+
+data$month <- factor(data$month, levels = 1:12, labels = month.abb)
+
+windows()
+ggplot(data, aes(x = month, y = 1-y_norm)) +
+  geom_boxplot()+
+  coord_polar() 
 
 # Simulate data
 one.year <- seq(from = 1, to = 365, by = 30)
@@ -22,10 +256,8 @@ Td <- 150
 # Generate year effects on Td
 uY_true    <- rep(0, n.years)
 uI_true    <- rep(0, n.inds)
-uI_true[1] <- -10
-uY_true[1] <- 30 
 Td_year_indv<- Td + uY_true[year.id] + uI_true[indv.id]       
-Td_year <- Td + uY_true[year.id]
+
 
 # simulate samples
 pf_true  <- pf_fun(kd = kd, Td = Td_year_indv, x = all.days)
